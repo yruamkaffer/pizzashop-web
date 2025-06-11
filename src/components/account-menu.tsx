aaeca0f81a537dfc6faa5b1d0295a7d47e6c0781
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-06-11 00:56:08 +0000

    Add files via upload

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -4,39 +4,53 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
 
 export function AccountMenu() {
 
-    const { data: profile } = useQuery({
+    const { data: profile, isLoading: isLoadingProfile } = useQuery({
         queryKey: ['profile'],
         queryFn: getProfile,
     })
 
-    const { data: managedRestaurant } = useQuery({
+    const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
         queryKey: ['managed-restaurant'],
         queryFn: getManagedRestaurant,
     })
 
 
     return (
+        <Dialog>
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="flex items-center gap-2 select-none">
-                    {managedRestaurant?.name} <ChevronDown className="w-4 h-4" /> 
+                    {isLoadingManagedRestaurant ? (
+                        <Skeleton className="h-4 w-40" />
+                    ) : managedRestaurant?.name} <ChevronDown className="w-4 h-4" /> 
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>{profile?.name}</span>
-                    <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                    {isLoadingProfile ? (
+                        <div className="space-y-1.5">
+                            <Skeleton className="h-4 w-32" />
+                            <Skeleton className="h-3 w-24"/>
+                        </div>
+                    ) : <>
+                            <span>{profile?.name}</span>
+                            <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                        </>}
                 </DropdownMenuLabel>
 
                 <DropdownMenuSeparator />
-
+                    <DialogTrigger asChild>
                 <DropdownMenuItem>
                     <Building className="w-4 h-4 mr-2" />
                     <span>Perfil da loja</span>
                 </DropdownMenuItem>
+                </DialogTrigger>
       
                 <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
                     <LogOut className="w-4 h-4 mr-2 text-rose-500 dark:text-rose-400" />
@@ -44,5 +58,8 @@ export function AccountMenu() {
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
+
+        <StoreProfileDialog />
+        </Dialog>
     )
 }
