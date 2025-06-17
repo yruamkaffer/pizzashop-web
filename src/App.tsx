b557dd8177c83b0d9b93896d7a110426801875e8
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-06-16 23:33:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import './global.css'
 import { router } from './routes'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
