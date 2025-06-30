cf9e7caebdba313e52bc73eeafa841b3b052d2d0
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-06-29 23:32:54 -0300

    Add files via upload

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -1,15 +1,92 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectTrigger, SelectItem, SelectValue } from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Search, X } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
+import z from "zod";
+
+const orderFiltersSchema = z.object({
+    orderId: z.string().optional(),
+    customerName: z.string().optional(),
+    status: z.string().optional(),
+});
+
+type OrderFiltersSchema = z.infer<typeof orderFiltersSchema>;
 
 export function OrderTableFilters() {
+const [searchParams, setSearchParams] = useSearchParams();
+
+const orderId = searchParams.get('orderId');
+const customerName = searchParams.get('customerName');
+const status = searchParams.get('status');
+
+    const { register, handleSubmit, control, reset } = useForm<OrderFiltersSchema>({
+        resolver: zodResolver(orderFiltersSchema),
+        defaultValues: {
+            orderId: orderId ?? '',
+            customerName: customerName ?? '',
+            status: status ?? 'all',
+        },
+})
+
+function handleFilter({ orderId, customerName, status}:OrderFiltersSchema){
+    setSearchParams(state => {
+      if (orderId) {
+        state.set('orderId', orderId);
+      } else {
+        state.delete('orderId');
+      }
+
+      if (customerName) {
+        state.set('customerName', customerName);
+      } else {
+        state.delete('customerName');
+      }
+
+      if (status) {
+        state.set('status', status);
+      } else {
+        state.delete('status');
+      }
+
+      state.set('page', '1')
+
+      return state;
+    });
+}
+
+function handleClearFilters() {
+    setSearchParams(state => {
+        state.delete('orderId');
+        state.delete('customerName');
+        state.delete('status');
+        state.set('page', '1');
+
+        return state;
+    }); 
+
+    reset({
+        orderId: '',
+        customerName: '',
+        status: 'all',
+    });
+}
+
     return (
-                    <form action="" className="flex items-center gap-2">
+                
+                <form onSubmit={handleSubmit(handleFilter)} action="" className="flex items-center gap-2">
                 <span className="text-sm font-semibold">Filtros:</span>
-                <Input placeholder="ID do pedido" className="h-8 w-auto"/>
-                <Input placeholder="Nome do cliente" className="h-8 w-[320px]"/>
-                <Select defaultValue="all">
+                <Input placeholder="ID do pedido" className="h-8 w-auto" {...register('orderId')}/>
+                <Input placeholder="Nome do cliente" className="h-8 w-[320px]" {...register('customerName')}/>
+
+                    <Controller
+                    name="status"
+                    control={control}
+                    render={({ field: {name, onChange, value, disabled}}) => { return(
+                                        <Select defaultValue="all" name={name} onValueChange={onChange} value={value} disabled={disabled}>
                     <SelectTrigger className="h-8 w-[180px]">
                         <SelectValue />
                     </SelectTrigger>
@@ -21,14 +98,16 @@ export function OrderTableFilters() {
                         <SelectItem value="delivering">Em entrega</SelectItem>
                         <SelectItem value="delivered">Entregue</SelectItem>                        
                     </SelectContent>
-                </Select>
+                </Select>)
+                    }}
+                    />
 
                 <Button type="submit" variant="secondary" size="xs">
                     <Search className="h-4 w-4 mr-2" />
                     Filtrar resultados
                 </Button>
 
-                 <Button type="button" variant="outline" size="xs">
+                 <Button onClick={handleClearFilters} type="button" variant="outline" size="xs">
                     <X className="h-4 w-4 mr-2"/>
                     Remover filtros
                 </Button>
